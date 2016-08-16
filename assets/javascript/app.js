689a9040f211f9570ac39ec2e394e1f3e4a86f61
JenPhD <jpemberton76@gmail.com>
2016-08-15 21:26:47 -0400

    basic working ajax call for Volunteer Match

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,109 +1,4 @@
-//Not exactly sure if the order for all of these things is correct, but I thought I would just 
-//start a skeleton with some thoughts about the things we will need.
-//LINK TO FIREBASE
-	//======================================================================================
-	// var config = {
-
-		//}
-
-		//var database = firebase.database();
-
-	//
-//SETUP VARIABLES
-	//================================================================================
-	// var volunteerMatchauthKey = "";
-	// var volqueryURLBase = ""; 
-	// var homeAwayauthKey = "";
-	// var homequeryURLBase = "";
-	// var googleFlightsauthKey = "";
-	// var flightqueryURLBase = "";
-	
-	//This array variable will hold the results we get from the user input after
-	//adding locations.
-
-	//Featured destinations (Just making these up right now. We can change them)
-	// var featureDestinations = ['New York ', 'LA', 'Chicago', 'Orlando', 'Seattle'];
-	
-//FUNCTIONS
-	//==================================================================================
-	//First load the inital array of featured destinations.
-	//Render the initial panel carousel on window load.
-	//Will need a for loop for this.
-
-	// Event listener for added cities. 
-	//This function handles events when the user inputs a new city not in our featured destinations
-
-	// $('#addDestination').on('click', function(){
-
-		// This line of code will grab the input from the textbox and trim any extra spaces
-
-		// The city from the textbox is then added to our array with destinations.push
-
-		//Clear the textboxes when done
-		
-		// We have this line so that users can hit "enter" instead of clicking on the button
-		// return false;
-
-		
-	// });
-
-	// AJAX calls for flights, home away, and volunteer
-	//====================================================================================================
-	//Create the queryURL. Use document.body. We need to do this for each API and
-	//should be sectioned off, but all within the on click function after the user
-	//has filled in destination and trip dates.
-		//Add volunteer opportunities
-
-		//Add available flights
-
-		//Add home away availability
-
-		//Storing all of these as properties of the destination and displayed in html trip panel
-
-	// $(document.body).on('click', '.destination', function() {
-		//first empty the div with any previous destination/trip information
-	   	
-		//Split and join replaces the spaces in a destination name with + so the URL works
-
-		//This builds the queryURL with the queryURLBase, the search term, and the authorization key. 
-		
-		//Use AJAX calls to retrieve the JSON from the APIs
-
-	    	//A variable array of objects returned from the API.
-
-	    		//for loop for the objects in the destinations array.
-	     	
-	     			//Create a new div for the destination
-
-	     			//Putting the destination and trip information into a nice div/panel		
-
-	// });//ending on-click submit destination button
-
-	//FIREBASE
-//===============================================================================
-	//Create firebase event for adding and savingtrips to the database and to the 
-	//html panel displaying trip information.
-	//database.ref().on("child_added", function(childSnapshot, prevChildKey){
-
-	//console.log(childSnapshot.val());
-
-	// Store everything into a variable with childSnapshot.val().
-	//Uploads all of the trip information to the database with 
-		//database.ref().push({})
-
-	//Add information into a final panel??? that displays all of the trip information
-
-
-	//});
-	
-
-//All of this is a rough outline. Just thoughts of the pieces we will need. Not sure
-//where the login code will go in this outline.
-
-//I think this is going to get more complicated as we realize there will be multiple flights at different times.
-//We need to think about whether we are going to just display what is available or give users the ability
-//to pick flights, pick their volunteer opportunity, and pick their lodging to save as their unique trip
-//or whether we are going to just display what is available/possible. But, these were initial thoughts I had.
+//FIREBASE=================================================================================
 $(document).ready (function(){
 	// Initialize Firebase
   var config = {
@@ -146,3 +41,102 @@ $(document).ready (function(){
 	  // An error happened.
 	});
 });
+
+//SETUP VARIABLES
+	//============================================================================================
+	var volunteerAPIkey="9560bbac6e597a4e08e3d82094ba5da2";
+	// authentication headers for API requests
+	var authenticationHeaders = {
+  		"X-WSSE": volunteerAPIkey,
+  		"Authorization": "WSSE profile='\ VolunteerHere\'" 
+	}
+
+	var queryURLBase = "http://www.volunteermatch.org/api/call?action=searchOpportunities";
+	
+	var testParameters = {
+    "location": "mountain view, ca",
+        "opportunityTypes": ["public", "private"]
+	};
+
+	// metaData for the API Key/site
+	var metaData = null;
+	//This array variable will hold the results we get from the user input after
+	//adding a location with the submit button.
+	
+	//Featured destinations (Just making these up right now. We can change them)
+	var featureDestinations = ['NYC', 'LA', 'Chicago', 'Austin', 'WDC'];
+
+	// we will use CORS headers to facilitate certain cross-domain requests (from JSFiddle only)
+	jQuery.support.cors = true;
+
+//FUNCTIONS
+	//=================================================================================================
+///This is just getting a response from Volunteer Match. Need to change the button names to match with HTML
+	//Create the query URL based on the chosen destination, either a feature destination
+	//or a destination they have added through a different destination pushed to our array.
+	//using document.body in case there are any scope problems
+
+	$(document.body).on('click', '#submitCity', function() {
+  		//First adding to array if it is a new city not in the carousel.
+  		// This line of code will grab the input from the textbox and trim any extra spaces
+		//var newCity = $('#city-input').val().trim();
+		//console.log(newCity);
+
+		// The city from the textbox is then added to our array
+		//featureDestinations.push(newCity);
+
+		//Clear the textbox when done
+		$('#cityinput').val(" ");
+	
+  		//first empty the div with any previous search results
+  		//$('#tripresults').empty();
+  		//Create a variable that is the search term, which is the name of the destination city
+  		//replace spaces in city names with a plus sign for a search
+  		//var searchTerm = $(this).data('.submitCity').split(' ').join('+');
+
+  		//Build the queryURL with the query with the query URL base and the search terms
+  		//(the city destination and trip dates)
+
+  		var queryURL = queryURLBase + "&key=" + volunteerAPIkey + "&query=" + JSON.stringify(testParameters);
+  		console.log("about to AJAX");
+
+  		//GET function to retrieve information about the volunteer opportunities from the volunteer match API
+  		//with an AJAX call
+  		$.ajax({url: queryURL, method: 'GET'})
+		.done(function(reponse) {
+		console.log(response);
+			
+			//Make a variable that stores the JSON from the volunteer match API
+			//var volMatch = response.data;
+			//console.log(volMatch)
+
+
+				//For loop for the city in the cities array.
+	    		//for (var j = 0; j < volMatch.length; j++) {
+	     	
+	     			//Create a new div for all of the trip data
+
+	     			//var trip = $('<div class="tripDiv">');
+					
+	     			//Add the volunteer opportunities, the google flights, the google hotels
+	      			//var volunteer = $('<p>').text("Here are your volunteer matches" + volMatch[j].whateverdata);
+	     			//console.log(volunteer);
+
+	     			//var flights = 
+
+	     			//var hotels = 
+
+	     			//Maybe get a nice image or gif to give trip itnerary some city visuals
+
+	    //  			var cityImages = $('<img>')
+	    //  			.addClass('cityimg');
+	     		
+	    //  			tripDiv.prepend(volunteer);
+	    //  			tripDiv.prepend(cityImages);
+	    //  			tripDiv.prepend(flights);
+	    //  			tripDiv.prepend(hotels);
+	    //  		}
+	    });	
+	    // We have this line so that users can hit "enter" instead of clicking on the button
+		return false;
+	});
